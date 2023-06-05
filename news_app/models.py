30ccc9c81dc52dc90a58b72d18e0e8f2df314faa
Fellaine <max.goncharevych@gmail.com>
2023-06-05 18:55:06 +0300

    Added model for article

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Source(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.TextField(null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    url = models.URLField(null=False, blank=False)
+    source_id = models.ForeignKey(Source, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
