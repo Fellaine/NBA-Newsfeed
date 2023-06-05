4b4ebde2a6f680864cf393fd57dfc36723423cab
Fellaine <max.goncharevych@gmail.com>
2023-06-05 12:58:05 +0300

    Made new scraper for r/NBA

--- news_app/utils.py
+++ news_app/utils.py
@@ -39,13 +39,13 @@ def get_nba_com_news():
         article_["title"], article_["content"], _ = article_el.text.split(
             "\n"
         )  # not using timestamp rn
-        article_["url"] = article_el.absolute_links.pop()  # get url from set set # noqa
+        article_["url"] = article_el.absolute_links.pop()  # get url from set set
         res.append(article_)
 
     return res
 
 
-def get_r_nba_news(limit: int = 6):
+def get_r_nba_news_throttled(limit: int = 6):
     r = requests.get(f"https://www.reddit.com/r/nba/top/.json?count={limit}")
     if r.status_code != 200:
         return 0
@@ -58,7 +58,9 @@ def get_r_nba_news(limit: int = 6):
             "content": post_data.get("selftext"),
             "url": post_data["url"],
         }
-        if article_["title"].startswith("[Post Game Thread]"):
+        if (article_["title"].startswith("[Post Game Thread]")) or (
+            article_["title"].startswith("GAME THREAD")
+        ):
             article_["content"] = ""
         res.append(article_)
 
@@ -95,10 +97,37 @@ def get_yahoo_news():
     return res
 
 
-# pretty = json.dumps(get_espn_news(), indent=4)
-# print(pretty)
-# print(get_espn_news(10))
-# print(get_nba_com_news())
+def get_r_nba_news():
+    session = HTMLSession()
+    r = session.get("https://www.reddit.com/r/nba/top/")
+    posts = r.html.find("._1poyrkZ7g36PawDueRza-J")
+    res = []
+    for post in posts:
+        title = post.find("h3")[0].text
+        content = ""
+        if not (title.startswith("[Post Game Thread]")) and not (
+            title.startswith("GAME THREAD")
+        ):
+            for paragraph in post.find("p"):
+                content += paragraph.text + r"\n"
+        url = post.find(".SQnoC3ObvgnGjWt90zD9Z")[0].absolute_links.pop()
+        res.append(
+            {
+                "title": title,
+                "content": content,
+                "url": url,
+            }
+        )
+    return res
+
+
+# print("ESPN NEWS: ")
+# print(json.dumps(get_espn_news(), indent=4))
+# print("NBA.COM NEWS: ")
 # print(json.dumps(get_nba_com_news(), indent=4))
+# # print("r/NBA NEWS: ")
+# # print(json.dumps(get_r_nba_news_throttled(), indent=4))
+# print("YAHOO NEWS: ")
+# print(json.dumps(get_yahoo_news(), indent=4))
+# print("r/NBA NEWS: ")
 # print(json.dumps(get_r_nba_news(), indent=4))
-print(json.dumps(get_yahoo_news(), indent=4))
