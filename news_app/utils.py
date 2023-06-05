820bc731acd3a053a204d8b98abab2f06fcbb358
Fellaine <max.goncharevych@gmail.com>
2023-06-05 09:10:26 +0300

    Added r/nba support

--- news_app/utils.py
+++ news_app/utils.py
@@ -7,21 +7,22 @@ from requests_html import HTMLSession
 
 def get_espn_news(limit: int = 6) -> list[dict] | Literal[0]:
     """
-    Get list of espn news with len = limit
-    If 404 return 0
+    Get list of espn news with len = limit.
+    If 404 return 0.
     """
     r = requests.get(
         f"http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news?limit={limit}"  # noqa
     )
     if r.status_code != 200:
         return 0
-    request = r.json()
+    response = r.json()
     res = []
-    article_ = {}
-    for article in request["articles"]:
-        article_["title"] = article["headline"]
-        article_["content"] = article["description"]
-        article_["url"] = article["links"]["web"]["href"]
+    for article in response["articles"]:
+        article_ = {
+            "title": article["headline"],
+            "content": article["description"],
+            "url": article["links"]["web"]["href"],
+        }
         res.append(article_)
     return res
 
@@ -38,9 +39,28 @@ def get_nba_com_news():
         article_["title"], article_["content"], _ = article_el.text.split(
             "\n"
         )  # not using timestamp rn
+        article_["url"] = article_el.absolute_links.pop()  # get url from set set # noqa
         res.append(article_)
 
-        # TODO : add url
+    return res
+
+
+def get_r_nba_news(limit: int = 6):
+    r = requests.get(f"https://www.reddit.com/r/nba/top/.json?count={limit}")
+    if r.status_code != 200:
+        return 0
+    response = r.json()
+    res = []
+    for post in response["data"]["children"]:
+        post_data = post["data"]
+        article_ = {
+            "title": post_data["title"],
+            "content": post_data.get("selftext"),
+            "url": post_data["url"],
+        }
+        if article_["title"].startswith("[Post Game Thread]"):
+            article_["content"] = ""
+        res.append(article_)
 
     return res
 
@@ -49,3 +69,5 @@ def get_nba_com_news():
 # print(pretty)
 # print(get_espn_news(10))
 # print(get_nba_com_news())
+# print(json.dumps(get_nba_com_news(), indent=4))
+print(json.dumps(get_r_nba_news(), indent=4))
