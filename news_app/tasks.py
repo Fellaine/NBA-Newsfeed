bbc18fb2523210ccaa76fc2d091543265917a0bd
Fellaine <max.goncharevych@gmail.com>
2023-06-06 17:34:07 +0300

    Made scraping into celery beat task

---
+++ news_app/tasks.py
@@ -0,0 +1,27 @@
+from django.db import IntegrityError
+
+from nbanews.celery import app
+
+from .models import Article
+from .utils import get_all_articles
+
+
+@app.task
+def scrape_articles():
+    all_articles = get_all_articles()
+    amount_of_already_saved = 0
+    for article in all_articles:
+        article_ = Article(
+            title=article["title"],
+            content=article["content"],
+            url=article["url"],
+            source_id=article["source_id"],
+        )
+        try:
+            article_.save()
+        except IntegrityError:
+            amount_of_already_saved -= 1
+
+    return (
+        f"Successfully saved {len(all_articles) + amount_of_already_saved} new articles"
+    )
