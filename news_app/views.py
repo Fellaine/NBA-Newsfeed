7cbc71dc1c36084c48d51d93272987a6e6fcafbb
Fellaine <max.goncharevych@gmail.com>
2023-06-16 17:33:33 +0300

    Added search support

--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import ListView
 
 from .models import Article
@@ -8,3 +9,13 @@ class ArticleListView(ListView):
     template_name = "templates/news_app/article-list.html"
     paginate_by = 32
     ordering = ["-id"]
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            object_list = self.model.objects.filter(
+                Q(title__icontains=query) | Q(content__icontains=query)
+            )
+        else:
+            object_list = self.model.objects.all()
+        return object_list
